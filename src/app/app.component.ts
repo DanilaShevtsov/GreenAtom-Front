166a6db53cf9eb8396630671ee629a100f03f7be
shevcovsfd <wegas.2001@gmail.com>
2022-11-26 02:15:06 +0300

    remove extend symbols

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
+
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/products';
 import { ProductsService } from './services/products.service';
-=======
-import { Component } from '@angular/core';
->>>>>>> 3fab1abc3ed425d68f4bf342da0561674ec4c44c
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-<<<<<<< HEAD
+
 export class AppComponent implements OnInit {
   constructor(private readonly productsService: ProductsService) {}
   loading = true;
@@ -26,8 +23,5 @@ export class AppComponent implements OnInit {
   title = 'greenatom';
 
   products: IProduct[] = [];
-=======
-export class AppComponent {
-  title = 'greenatom';
->>>>>>> 3fab1abc3ed425d68f4bf342da0561674ec4c44c
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
+
 import { HttpClientModule } from '@angular/common/http';
-=======
->>>>>>> 3fab1abc3ed425d68f4bf342da0561674ec4c44c
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { ProductComponent } from './components/product/product.component';
 import { ProductsService } from './services/products.service';
 import { CreateProductComponent } from './components/create-product/create-product.component';
@@ -28,18 +27,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule
   ],
   providers: [ProductsService],
-=======
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
->>>>>>> 3fab1abc3ed425d68f4bf342da0561674ec4c44c
   bootstrap: [AppComponent]
 })
 export class AppModule { }
