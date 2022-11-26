88b4c57f17d60487469e23d805aa850712e953b1
shevcovsfd <wegas.2001@gmail.com>
2022-11-27 01:38:59 +0300

    add vacancy creating and authorization

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { IAdminAnswer } from 'src/app/models/admin-answer';
 import { AdminService } from 'src/app/services/admin.service';
 
@@ -9,6 +10,21 @@ import { AdminService } from 'src/app/services/admin.service';
 })
 export class AdminComponent implements OnInit {
   answers: IAdminAnswer[] = [];
+  authorized = false;
+  jwt:string;
+
+  vacancyForm = new FormGroup({
+    title: new FormControl<string>(''),
+    preview: new FormControl<string>(''),
+    description: new FormControl<string>(''),
+    city: new FormControl<string>('')
+  });
+
+  authForm = new FormGroup({
+    username: new FormControl<string>(''),
+    password: new FormControl<string>('')
+  });
+
   constructor(private readonly adminService:AdminService) { }
 
   ngOnInit(): void {
@@ -20,5 +36,33 @@ export class AdminComponent implements OnInit {
 
   redirectQR(vacancyId:string) {
     console.log(vacancyId);
+  }
+
+  createVacancy() {
+    const title = this.vacancyForm.value.title;
+    const preview = this.vacancyForm.value.preview;
+    const description = this.vacancyForm.value.description;
+    const city = this.vacancyForm.value.city;
+
+    this.adminService.createVacancy({
+      title: title as string,
+      preview: preview as string,
+      description: description as string,
+      city: city as string,
+    }, this.jwt).subscribe();
+  }
+
+  auth() {
+    const username = this.authForm.value.username;
+    const password = this.authForm.value.password;
+
+    this.adminService.auth({
+      username: username as string,
+      password: password as string,
+    }).subscribe((resp) => {
+      this.jwt = resp.access_token;
+      console.log(this.jwt);
+      this.authorized = true;
+    });
   }
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IAdminAnswer } from '../models/admin-answer';
+import { IAuth } from '../models/auth';
+import { ICreateVacancy } from '../models/create-vacancy';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,20 @@ export class AdminService {
 
   getAnswers(): Observable<IAdminAnswer[]> {
     const resp = this.http.get<IAdminAnswer[]>(`${environment.backUrl}/quiz/get-answers`);
+    return resp;
+  }
+
+  createVacancy(body: ICreateVacancy, jwt:string): Observable<ICreateVacancy> {
+    const resp = this.http.post<ICreateVacancy>(`${environment.backUrl}/vacancy/store`, body, {
+      headers: {
+        Authorization: 'Bearer ' + jwt
+      }
+    });
+    return resp;
+  }
+  
+  auth(body: IAuth): Observable<any> {
+    const resp = this.http.post<any>(`${environment.backUrl}/admin/login`, body);
     return resp;
   }
 }
