a51864f1081bcb5364aa6577f87656d69a194f5a
shevcovsfd <wegas.2001@gmail.com>
2022-11-26 04:53:26 +0300

    css connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,15 @@ import { ProjectService } from './services/projects.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-
 export class AppComponent implements OnInit {
   constructor(private readonly projectService: ProjectService) {}
 
   ngOnInit(): void {
     this.projectService.getAll().subscribe((projects) => {
       this.projects = projects;
-      console.log(this.projects);
     });
    }
   projects: IProject[] = [];
